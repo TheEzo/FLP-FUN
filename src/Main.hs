952e5aec1dcbc5dc8ff4da8c3bb0297b835ec221
TheEzo <willaschek.t@gmail.com>
2020-03-28 22:12:42 +0100

    a

--- src/Main.hs
+++ src/Main.hs
@@ -4,10 +4,18 @@
 -- Rok:     2020
 
 import System.Environment
+import Data.List.Split
 
 main :: IO ()
 main = do
-    x <- getArgs
-    print x
-    -- args ["-h"] = print "ahoj"
+    args <- getArgs
+    let file = last args
+    case head args of "-i" -> print "-i"
+                      "-1" -> print "-1"
+                      "-2" -> print "-2"
+                      _    -> error "Unknown argument..."
     
+    f <- readFile file
+    let l = lines f
+    print (splitOn "," (head l))
+
